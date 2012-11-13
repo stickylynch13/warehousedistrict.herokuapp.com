31953bafa4d73503a6e7663e004d6c22f997960c
Keith Thompson <keith@brilliantfantastic.com>
2012-11-12 23:49:45 -0500

    Added some sample restaurants and real estate businesses to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@ User.create({username: 'keith', password: 'password'})
 User.create({username: 'donmiller', password: 'donmiller'})
 User.create({username: 'andrew', password: 'andrew'})
 
+# Create general businesses
 10.times do
   Business.create({
     name: Faker::Company.name,
@@ -16,5 +17,33 @@ User.create({username: 'andrew', password: 'andrew'})
     website: Faker::Internet.domain_name,
     member: true,
     logo_url: Faker::Internet.domain_name
+  })
+end
+
+# create some restaurants
+5.times do
+  Business.create({
+    name: Faker::Company.name,
+    description: Faker::Lorem.sentences(3),
+    address: Faker::Address.street_address,
+    phone: Faker::PhoneNumber.phone_number,
+    website: Faker::Internet.domain_name,
+    member: true,
+    logo_url: Faker::Internet.domain_name,
+    category_id: 1
+  })
+end
+
+# create some real estate businesses
+6.times do
+  Business.create({
+    name: Faker::Company.name,
+    description: Faker::Lorem.sentences(3),
+    address: Faker::Address.street_address,
+    phone: Faker::PhoneNumber.phone_number,
+    website: Faker::Internet.domain_name,
+    member: true,
+    logo_url: Faker::Internet.domain_name,
+    category_id: 2
   })
 end
